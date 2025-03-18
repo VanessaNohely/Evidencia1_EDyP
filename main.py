ab6a24cdc22b302b3362c38107c13115d7b4b797
Vanessa Nohely Arrambide Escamilla <arrambidenohely2003@gmail.com>
2025-03-17 22:16:02 -0600

    Importacion de librerias

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-#Archivo evidencia 1- Vanessa Nohely Arrambide Escamilla-1980877
+
+"""
+Evidencia1_EDyP
+
+Autor: Vanessa Nohely Arrambide Escamilla - 1980877"""
+
+"""Librerías y Modulos"""
+import csv
+import os
+import pandas as pd
+import uuid
+from datetime import datetime
+
 
 def registrar_nota():
     pass
@@ -15,7 +27,9 @@ def cancelar_nota():
 def recuperar_nota():
     pass
  
+"""Menú Principal"""
 def menu_principal():
+    """Despliega el menú principal"""
     while True:
         print("\nMenú Principal")
         print("1. Registrar una nota")
@@ -24,27 +38,25 @@ def menu_principal():
         print("4. Recuperar una nota")
         print("5. Salir del sistema")
         opcion = input("Seleccione una opción: ")
-       
         if opcion == "1":
             registrar_nota()
         elif opcion == "2":
-            print("\n1. Consulta por período")
+            print("\nMenú Principal>Consultas y reportes")
+            print("1. Consulta por período")
             print("2. Consulta por folio")
-            print("3. Regresar al menú principal")
             sub_opcion = input("Seleccione una opción: ")
             if sub_opcion == "1":
-                consultar_notas_por_periodo()
+                consultar_por_periodo()
             elif sub_opcion == "2":
-                consultar_nota_por_folio()
+                consultar_por_folio()
         elif opcion == "3":
             cancelar_nota()
         elif opcion == "4":
             recuperar_nota()
         elif opcion == "5":
-            print("Saliendo del sistema...")
+            print("Saliendo...")
             break
         else:
-            print("Opción no válida, intente de nuevo.")
+            print("Opción inválida.")
 
 menu_principal()
- 
