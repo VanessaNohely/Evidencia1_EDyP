01d6f4b4491a028e4b908d9ac1c4ebf1f84d62eb
Vanessa Nohely Arrambide Escamilla <arrambidenohely2003@gmail.com>
2025-03-17 20:28:11 -0600

    Correccion de menu
    Correcion del esqueleto del menu

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 #Archivo evidencia 1- Vanessa Nohely Arrambide Escamilla-1980877
+
 def registrar_nota():
     pass
  
-def consultas_reportes():
+def consultar_notas_por_periodo():
+    pass
+ 
+def consultar_nota_por_folio():
     pass
  
 def cancelar_nota():
@@ -20,16 +24,27 @@ def menu_principal():
         print("4. Recuperar una nota")
         print("5. Salir del sistema")
         opcion = input("Seleccione una opción: ")
+       
         if opcion == "1":
             registrar_nota()
         elif opcion == "2":
-            consultas_reportes()
+            print("\n1. Consulta por período")
+            print("2. Consulta por folio")
+            print("3. Regresar al menú principal")
+            sub_opcion = input("Seleccione una opción: ")
+            if sub_opcion == "1":
+                consultar_notas_por_periodo()
+            elif sub_opcion == "2":
+                consultar_nota_por_folio()
         elif opcion == "3":
             cancelar_nota()
         elif opcion == "4":
             recuperar_nota()
         elif opcion == "5":
-            print("Saliendo del sistema.")
+            print("Saliendo del sistema...")
             break
         else:
-            print("Opción no válida. Intente de nuevo.")
+            print("Opción no válida, intente de nuevo.")
+
+menu_principal()
+ 
