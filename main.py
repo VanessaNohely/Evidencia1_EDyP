19bf6caa0f4070b957a75c1e8871db4493b6362c
Vanessa Nohely Arrambide Escamilla <arrambidenohely2003@gmail.com>
2025-03-17 20:19:37 -0600

    Creacion de menu

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
 #Archivo evidencia 1- Vanessa Nohely Arrambide Escamilla-1980877
+def registrar_nota():
+    pass
+ 
+def consultas_reportes():
+    pass
+ 
+def cancelar_nota():
+    pass
+ 
+def recuperar_nota():
+    pass
+ 
+def menu_principal():
+    while True:
+        print("\nMenú Principal")
+        print("1. Registrar una nota")
+        print("2. Consultas y reportes")
+        print("3. Cancelar una nota")
+        print("4. Recuperar una nota")
+        print("5. Salir del sistema")
+        opcion = input("Seleccione una opción: ")
+        if opcion == "1":
+            registrar_nota()
+        elif opcion == "2":
+            consultas_reportes()
+        elif opcion == "3":
+            cancelar_nota()
+        elif opcion == "4":
+            recuperar_nota()
+        elif opcion == "5":
+            print("Saliendo del sistema.")
+            break
+        else:
+            print("Opción no válida. Intente de nuevo.")
